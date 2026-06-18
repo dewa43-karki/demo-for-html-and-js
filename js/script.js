5185d16e6724be9b18c19b778dd01ad74fb78299
Dewa43 Karki <karkidewa@gmail.com>
2026-06-18 17:21:59 +0545

    Some changes

---
+++ js/script.js
@@ -0,0 +1,10 @@
+const profile{
+    username: "dewakarki";
+    isfollow: true;
+    post: 0;
+    followers: 85;
+    following: 49;
+    bio: "My eyess!!! My eyes!!!"
+};
+
+console.log(profile);
