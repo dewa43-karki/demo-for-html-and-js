232f61be6b7fb10c27a96a185c89b0460d148150
Dewa43 Karki <karkidewa@gmail.com>
2026-06-22 18:40:27 +0545

    Add L3

--- js/script.js
+++ js/script.js
@@ -63,3 +63,9 @@ while( usernum !== num) {
 }
 
 console.log("Well you did it");
+
+
+//strings
+let name= prompt(" Enter your name:");
+let username= "@" + name + name.length;
+console.log("Your username is", username);
