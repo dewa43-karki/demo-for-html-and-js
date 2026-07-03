5c05fb1f5d638eba885df4d6329d226edc3e7bb8
Dewa43 Karki <karkidewa@gmail.com>
2026-07-03 18:10:30 +0545

    L4

--- js/script.js
+++ js/script.js
@@ -10,6 +10,7 @@ const profile = {
 console.log(profile["bio"]);
 
 
+
 //2nd Operators and Conditional sentences
 const product = {
     title: "Pen",
@@ -20,7 +21,6 @@ const product = {
 console.log(product);
 
 
-//3rd
 let a=8;
 if (a%4 == 0){
     console.log(a, "is a multiple of 4.");
@@ -30,7 +30,6 @@ else {
 }
 
 
-//4th
 let score=prompt("Enter your score: ");
 if (score>=90 && score<=100) {
     console.log("A");
@@ -49,7 +48,8 @@ else{
 }
 
 
-//5th Loops and Strings
+
+//3rd Loops and Strings
 for (let i = 0; i <= 100; i++) {
     if (i % 2 == 0) {
         console.log("num", i);
@@ -65,13 +65,13 @@ while( usernum !== num) {
 console.log("Well you did it");
 
 
-//strings
 let name= prompt(" Enter your name:");
 let username= "@" + name + name.length;
 console.log("Your username is", username);
 
 
-//arrays
+
+//4th Arrays
 let marks= [85, 97, 44, 37, 76, 60];
 let sum=0;
 for( i=0; i<marks.length; i++) {
@@ -87,6 +87,7 @@ console.log("The average marks of the class is", average);
 // .......
 // console.log(`avg marks of the class = ${avg}`);
 
+
 let prices = [250, 645, 300, 900, 50];
 let finalprices;
 for ( i=0; i<prices.length; i++ ){
@@ -94,7 +95,50 @@ for ( i=0; i<prices.length; i++ ){
     console.log(finalprices);
 }
 
+
 let company = ["Bloomberg", "Microsoft", "Google", "IBM", "Netflix", "Brave"];
 // company.shift();
 // company.splice(4,1, "PrimeVideos");
 company.push("Amazon");
+
+
+
+//5th Functions & Methods
+function vowelCount(string){
+   let vowCount=0;
+   for (let i of string){
+      if (i=="a" || i=="e" || i=="i" || i=="o" || i=="u" || i=="A" || i=="E" || i=="I" || i=="O" || i=="U"){
+         vowCount++;
+        }
+    }
+    console.log("The total number of vowels in the text is", vowCount);
+}
+
+
+let Vowelcount = (string) => {
+    let Vowelcou=0;
+   for (let i of string){
+      if (i=="a" || i=="e" || i=="i" || i=="o" || i=="u" || i=="A" || i=="E" || i=="I" || i=="O" || i=="U"){
+         Vowelcou++;
+        }
+    }
+    return Vowelcou;
+}
+
+
+let array=[2,4,6,8,10];
+array.forEach(function square(val){
+    console.log(val*val);
+});
+
+let arr= [12, 34, 56, 78, 90];          // another way to write the above function using arrow function
+arr.forEach((val) => {
+    console.log(val * val);
+});
+
+let arrays= [1, 2, 3, 4, 5];           // another way to write the above function using callback function
+let sq = (val) => {
+    console.log(val * val);
+}
+arrays.forEach(sq);
+
